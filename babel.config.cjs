0ea521d13313675aaa9c2b0ec2843287499e97fe
Colin Rotherham <work@colinr.com>
2024-06-04 16:07:43 +0100

    Prefer JavaScript config files

---
+++ babel.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '20'
+        }
+      }
+    ]
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '~': '.'
+        }
+      }
+    ]
+  ],
+  env: {
+    development: {
+      sourceMaps: 'inline',
+      retainLines: true
+    }
+  }
+}
