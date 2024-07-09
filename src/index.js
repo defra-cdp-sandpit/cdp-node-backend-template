ae6fc0ad85890c986696f4d7111bf7bdfd6ebace
Ben Chidgey <ben@feedmypixel.com>
2024-07-09 09:39:15 +0100

    Add high level await to server creation

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { createLogger } from '~/src/helpers/logging/logger.js'
 import { startServer, stopServer } from '~/src/helpers/server/index.js'
 
 const logger = createLogger()
-const serverPromise = startServer()
+const server = await startServer()
 
 process.on('unhandledRejection', (error) => {
   logger.info('Unhandled rejection')
   logger.error(error)
   process.exitCode = 1
 })
-process.on('SIGINT', () => serverPromise.then(stopServer))
-process.on('SIGTERM', () => serverPromise.then(stopServer))
+process.on('SIGINT', () => stopServer(server))
+process.on('SIGTERM', () => stopServer(server))
