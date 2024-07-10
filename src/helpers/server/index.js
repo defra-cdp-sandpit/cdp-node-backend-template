161ca6ae5fa0cb99ae9dc21c6de7a0d711860d97
chotai <chotai@users.noreply.github.com>
2024-07-10 14:39:20 +0100

    Replace stop-server with hapi-pulse

--- src/helpers/server/index.js
+++
@@ -1,4 +0,0 @@
-import { startServer } from '~/src/helpers/server/start-server.js'
-import { stopServer } from '~/src/helpers/server/stop-server.js'
-
-export { startServer, stopServer }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import process from 'node:process'
 import { createLogger } from '~/src/helpers/logging/logger.js'
-import { startServer, stopServer } from '~/src/helpers/server/index.js'
+import { startServer } from '~/src/helpers/start-server.js'
 
-const logger = createLogger()
-const server = await startServer()
+await startServer()
 
 process.on('unhandledRejection', (error) => {
+  const logger = createLogger()
   logger.info('Unhandled rejection')
   logger.error(error)
   process.exitCode = 1
 })
-process.on('SIGINT', () => stopServer(server))
-process.on('SIGTERM', () => stopServer(server))
