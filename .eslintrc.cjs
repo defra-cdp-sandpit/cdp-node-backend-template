d37c3dd16bd727da408e0a12966c6c593b25fa5e
Ben Chidgey <ben@feedmypixel.com>
2024-08-23 15:52:55 +0100

    Inclyde .ts files in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,7 @@ module.exports = {
       env: {
         browser: false
       },
-      files: ['**/*.{cjs,js}'],
+      files: ['**/*.{cjs,js,ts}'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
         ecmaVersion: 'latest',
