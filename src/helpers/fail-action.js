618797ac3a4306524904b7ffd058d9ec2acc1029
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-09-26 11:29:26 +0100

    addresses sonarcloud issue

--- src/helpers/fail-action.js
+++ src/helpers/fail-action.js
@@ -4,12 +4,12 @@ const logger = createLogger()
 
 /**
  *
- * @param { import('@hapi/hapi').Request } request
+ * @param { import('@hapi/hapi').Request } _request
  * @param { import('@hapi/hapi').ResponseToolkit } _h
  * @param { Error|undefined } error
  * @returns { never }
  */
-export function failAction(request, _h, error) {
+export function failAction(_request, _h, error) {
   logger.error(error, error?.message)
   throw error
 }
